a7471789c354757368c669af7c05ee1e928bd6ef
Riyad <Riyad9876@gmail.com>
2023-09-05 10:31:38 -0400

    /:number route

--- server.js
+++ server.js
@@ -8,7 +8,18 @@ const PORT = 3001
 
 
 app.get('/', (req, res) => {
-    res.send(`<h1>99 bottles of milk on the wall</h1>`)
+    res.send(`<h1>99 bottles of milk on the wall</h1> <a href='localhost:${PORT}/98'>Take one down, pass it around</a>`)
+})
+
+
+app.get('/:number', (req, res) => {
+    let number = req.params.number
+    if (number > 0){
+        res.send(`<h1>${number} bottles of milk on the wall</h1><a href='localhost:${PORT}/${number - 1}'>Take one down, pass it around</a> `)
+    }
+    else {
+        res.send('<h1>Input a number above 0</h1>')
+    }
 })
 
 
